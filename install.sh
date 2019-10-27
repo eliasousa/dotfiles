a68a058159c79149375698fdd2d2a650a63ca135
Elias Sousa <humb.eliasj@gmail.com>
2019-10-27 10:28:21 -0300

    Change brewfile

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 
 echo "Setting up your Mac..."
 
+echo "Installing Brew..."
 # Check for Homebrew and install if we don't have it
 if test ! $(which brew); then
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
@@ -10,35 +11,46 @@ fi
 # Update Homebrew recipes
 brew update
 
+echo "Installing Brew dependencies..."
 # Install all our dependencies with bundle (See Brewfile)
 brew tap homebrew/bundle
 brew bundle
 
+echo "Installing OhMyZsh..."
 # Install OhMyZsh
 curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh
 
+echo "Adding development directories..."
 # Create a Projects directory
 mkdir $HOME/Projects
+mkdir $HOME/Projects/jaya
+mkdir $HOME/Projects/personal
 
+echo "Replacing .zshrc..."
 # Removes .zshrc from $HOME (if it exists) and symlinks the .zshrc file from the .dotfiles
 rm -rf $HOME/.zshrc
 ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
 
+echo "Installing zsh plugins..."
 # Clone oh-my-zsh plugins
 git clone https://github.com/zsh-users/zsh-autosuggestions "$DOTFILES/plugins/zsh-autosuggestions"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$DOTFILES/plugins/zsh-syntax-highlighting"
 
+echo "Installing zsh spaceship theme..."
 # Clone and setup oh-my-zsh theme
 git clone https://github.com/denysdovhan/spaceship-prompt.git "$DOTFILES/themes/spaceship-prompt"
 ln -s "$DOTFILES/themes/spaceship-prompt/spaceship.zsh-theme" "$DOTFILES/themes/spaceship.zsh-theme"
 
+echo "Setting up mackup..."
 # Symlink the Mackup config file to the home directory
 ln -s $HOME/.dotfiles/.mackup.cfg $HOME/.mackup.cfg
 
+echo "Setting macOS preferences..."
 # Set macOS preferences
 # We will run this last because this will reload the shell
 source .macos
 
+echo "Installing ASDF plugins..."
 # Install ASDF plugins
 asdf plugin-add ruby
 asdf plugin-add elixir
